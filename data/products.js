e2ebc11998507b5532701bd6e3195775c676df87
piyushNegiDev <piyushnegi381@gmail.com>
2025-08-03 02:10:22 +0530

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -97,11 +97,13 @@ export function loadProductsFetch() {
         return new Product(productDetails);
       });
       console.log("load products");
+    })
+    .catch((error) => {
+      console.log("Unexpected error. Please try again later.");
     });
 
   return promise;
 }
-
 /*
 loadProductsFetch().then(() => {
   console.log("next step");
@@ -120,6 +122,10 @@ export function loadProducts(fun) {
     });
     console.log("load products");
     fun();
+  });
+
+  xhr.addEventListener("error", (error) => {
+    console.log("Unexpected error. Please try again later.");
   });
 
   xhr.open("GET", "https://supersimplebackend.dev/products");
